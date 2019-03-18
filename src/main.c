db4faf370b95972b9cf8ebe7c733d1c8f5691543
firgaty <felix.desmaretz@protonmail.com>
2019-03-18 13:35:56 +0100

    update

--- src/main.c
+++ src/main.c
@@ -1,24 +1,23 @@
 /**
  * @file main.c
- * @author Félix Desmaretz 
- * @brief 
+ * @author Félix Desmaretz
+ * @brief
  * @version 0.1
  * @date 2019-03-11
- * 
+ *
  * @copyright Copyright (c) 2019
- * 
+ *
  */
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "msg.h"
 #include "test.h"
 
-int main(int argc, char const *argv[])
-{
-    srand(time(NULL));
+int main(int argc, char const *argv[]) {
+  srand(time(NULL));
 
-    test_msg();
+  test_msg();
 
-    return 0;
+  return 0;
 }
