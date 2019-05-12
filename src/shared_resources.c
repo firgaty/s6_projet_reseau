cb62cff80f77783d16e9b20102d6be5c28da775c
firgaty <felix.desmaretz@protonmail.com>
2019-05-12 22:39:31 +0200

    deadlock

--- src/shared_resources.c
+++ src/shared_resources.c
@@ -1,43 +1,47 @@
 #include "shared_resources.h"
 
+neighbour_map_t* g_cur_neighbours = NULL;
+neighbour_map_t* g_pot_neighbours = NULL;
+data_map_t* g_msg_map = NULL;
+dllist_t* g_msg_list = NULL;
+
 neighbour_map_t* get_cur_neighbours() {
   pthread_mutex_lock(&cur_neighbours_lock);
 
-  static neighbour_map_t* m = NULL;
-  if (m == NULL) {
-    m = new_neighbour_map();
+  if (g_cur_neighbours == NULL) {
+    g_cur_neighbours = new_neighbour_map();
   }
-  return m;
+  return g_cur_neighbours;
 }
 
 neighbour_map_t* get_pot_neighbours() {
   pthread_mutex_lock(&pot_neighbours_lock);
 
-  static neighbour_map_t* m = NULL;
-  if (m == NULL) {
-    m = new_neighbour_map();
+  // static neighbour_map_t* m = NULL;
+  if (g_pot_neighbours == NULL) {
+    g_pot_neighbours = new_neighbour_map();
   }
-  return m;
+  return g_pot_neighbours;
 }
 
 data_map_t* get_msg_map() {
   pthread_mutex_lock(&msg_map_lock);
 
-  static data_map_t* m = NULL;
-  if (m == NULL) {
-    m = new_data_map();
+  // static data_map_t* m = NULL;
+  if (g_msg_map == NULL) {
+    g_msg_map = new_data_map();
   }
-  return m;
+  return g_msg_map;
 }
 
 dllist_t* get_msg_list() {
   pthread_mutex_lock(&msg_list_lock);
 
-  static dllist_t* l = NULL;
-  if (l == NULL) {
-    l = new_dllist(DLL_STRING);
+  // static dllist_t* l = NULL;
+  if (g_msg_list == NULL) {
+    g_msg_list = new_dllist(DLL_STRING);
   }
-  return l;
+  return g_msg_list;
 }
 
 void new_client_id() {
